19b190da0ad09c2753d006e963213ab4b5b779c0
reeeho-s <91420022+reeeho-s@users.noreply.github.com>
2021-10-27 07:39:21 +0000

    Add bookpage

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,39 @@
+class BooksController < ApplicationController
+  before_action :authenticate_user!
+  
+  def index
+    @book = Book.new
+    @books = Book.all
+  end
+  
+  def create
+    book = Book.new(book_params)
+    if @book.save
+      redirect_to book_path(@book.id), notice:'You have creatad book successfully.'
+    else
+      render :index
+    end
+  end
+  
+  def show
+  end
+  
+  def edit
+    @book = Book.find(params[:id])
+  end
+  
+  def update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    redirect_to 
+  end
+  
+  def destroy
+  end
+
+  private
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+  
+end
